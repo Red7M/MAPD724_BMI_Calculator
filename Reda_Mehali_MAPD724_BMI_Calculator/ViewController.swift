26dd5ea7b40897a30071bbd666fe5dbb66d204b5
Red7M <reda.mehali@gmail.com>
2021-03-28 18:00:51 -0400

    Initial Commit

---
+++ Reda_Mehali_MAPD724_BMI_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reda_Mehali_MAPD724_BMI_Calculator
+//
+//  Created by Reda Mehali on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
